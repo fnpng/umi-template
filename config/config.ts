8a34cf4feeee393542902bb2a7bb3611f0a04e67
Polar <91142488+polarss@users.noreply.github.com>
2023-12-14 15:29:29 +0800

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,32 +1,17 @@
 import { defineConfig } from '@umijs/max';
 import routes from '../src/routes';
-import settings from '../src/settings.json';
 import proxy from './proxy';
 
 export default defineConfig({
-  antd: {
-    theme: {
-      token: {
-        colorPrimary: settings.themeColor,
-      },
-    },
-    appConfig: {
-      message: {
-        maxCount: 3,
-      },
-    },
-  },
+  antd: {},
   define: {
     'process.env.UMI_ENV': 'dev',
     'process.env.API_PROXY_URL': 'http://localhost',
   },
   proxy,
-  model: {},
-  initialState: {},
-  // request: {},
   layout: {},
   routes,
   npmClient: 'pnpm',
   tailwindcss: {},
-  theme: { '@primary-color': '#1DA57A' },
+  esbuildMinifyIIFE: true,
 });
